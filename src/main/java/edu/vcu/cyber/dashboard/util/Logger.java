5e8142fc36ea650d028dcf2f0134a1dd4e69d403
Brandon Simon <simonbj@vcu.edu>
2019-02-18 11:33:22 -0500

    updated gitignore and some logging stuff

--- src/main/java/edu/vcu/cyber/dashboard/util/Logger.java
+++ src/main/java/edu/vcu/cyber/dashboard/util/Logger.java
@@ -2,32 +2,32 @@ package edu.vcu.cyber.dashboard.util;
 
 public class Logger
 {
-	public void log(String format, Object... vals)
+	public static void log(String format, Object... vals)
 	{
 		System.out.printf(format, vals);
 	}
 
-	public void log(String text)
+	public static void log(String text)
 	{
 		System.out.print(text);
 	}
 
-	public void err(String format, Object... vals)
+	public static void err(String format, Object... vals)
 	{
 		System.err.printf(format, vals);
 	}
 
-	public void err(String text)
+	public static void err(String text)
 	{
 		System.err.println(text);
 	}
 
-	public void debug(String format, Object... vals)
+	public static void debug(String format, Object... vals)
 	{
 		System.out.printf(format, vals);
 	}
 
-	public void debug(String text)
+	public static void debug(String text)
 	{
 		System.out.print(text);
 	}
